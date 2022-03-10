2e8ee5ed49dfc9c6a7b39f015d76e7295eee9320
Sébastien <snicou.pro@gmail.com>
2022-03-10 11:56:33 +0100

    add: header.php

--- wp-content/themes/lazuli-starter/header.php
+++ wp-content/themes/lazuli-starter/header.php
@@ -2,19 +2,42 @@
 
 <header id="masthead" class="site-header" role="banner">
 
-    <nav class="navbar navbar-expand-lg navbar-light" role="navigation">
-        <div class="container">
-
-			<?php get_template_part( 'template-parts/header/custom-logo' ); ?>
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#primary-menu-navigation"
-                    aria-controls="primary-menu-navigation" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Do Not Eat</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <?php get_template_part( 'template-parts/header/custom-logo' ); ?>
             </button>
-
-			<?php get_template_part( 'template-parts/header/menu' ); ?>
-
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Notre concept</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Nos boutiques</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Nous rejoindre</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Nous contacter</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown link
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <?php get_template_part( 'template-parts/header/menu' ); ?>
         </div>
     </nav>
 
