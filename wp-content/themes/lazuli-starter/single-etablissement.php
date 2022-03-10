514166b4ed04305b4792a054b836d2e4f01a1f4b
Victor grlr <victor.grollier@gmail.com>
2022-03-10 13:34:46 +0100

    update: single-etablissement

--- wp-content/themes/lazuli-starter/single-etablissement.php
+++ wp-content/themes/lazuli-starter/single-etablissement.php
@@ -3,7 +3,11 @@
 
        <h1><?php the_title(); ?></h1>
 
-
+        <?php if ($image = get_field('image_etablissement')) : ?>
+            <div class="image-etablissement"'>
+                <?php echo wp_get_attachment_image($image['id'], 'large'); ?>
+            </div>
+        <?php endif; ?>
 
     </main><!-- #main -->
 <?php get_footer();
