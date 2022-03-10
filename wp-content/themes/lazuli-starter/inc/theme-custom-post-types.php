ed8f4adca1d184cb66c920790f3d7fafa84305ec
Victor grlr <victor.grollier@gmail.com>
2022-03-10 10:11:27 +0100

    add: recrutement_cpt and icon

--- wp-content/themes/lazuli-starter/inc/theme-custom-post-types.php
+++ wp-content/themes/lazuli-starter/inc/theme-custom-post-types.php
@@ -1,6 +1,6 @@
 <?php
 
-function lazuli_custom_post_types() {
+function etablissement_custom_post_types() {
 	$cpt = array(
 		'labels'              => array(
 			'name'          => __( 'Etablissements', 'lazuli' ),
@@ -15,10 +15,33 @@ function lazuli_custom_post_types() {
 		'exclude_from_search' => false,
 		'rewrite'             => array( 'slug' => 'etablissement', 'with_front' => true ),
 		'supports'            => array( 'title', 'editor', 'thumbnail', 'page-attributes', 'excerpt' ),
-		'menu_icon'           => 'dashicons-welcome-learn-more',
+		'menu_icon'           => 'dashicons-store',
 	);
 
 	register_post_type( 'etablissement', $cpt );
 }
 
-add_action( 'init', 'lazuli_custom_post_types' );
+add_action( 'init', 'etablissement_custom_post_types' );
+
+function recrutement_custom_post_types() {
+    $cpt = array(
+        'labels'              => array(
+            'name'          => __( 'Recrutements', 'lazuli' ),
+            'singular_name' => __( 'Recrutement', 'lazuli' ),
+        ),
+        'show_ui'             => true,
+        'show_admin_column'   => true,
+        'show_in_rest'        => true,
+        'hierarchical'        => true,
+        'public'              => true,
+        'has_archive'         => true,
+        'exclude_from_search' => false,
+        'rewrite'             => array( 'slug' => 'recrutement', 'with_front' => true ),
+        'supports'            => array( 'title', 'editor', 'thumbnail', 'page-attributes', 'excerpt' ),
+        'menu_icon'           => 'dashicons-businessman',
+    );
+
+    register_post_type( 'recrutement', $cpt );
+}
+
+add_action( 'init', 'recrutement_custom_post_types' );
