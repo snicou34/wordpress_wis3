8f2eaa6a09d4f39c42e4a71a8873c3f4c3cdd81a
Sébastien <snicou.pro@gmail.com>
2022-03-10 13:42:34 +0100

    update: single-recrutement

--- wp-content/themes/lazuli-starter/single-recrutement.php
+++ wp-content/themes/lazuli-starter/single-recrutement.php
@@ -3,7 +3,11 @@
 
         <h1><?php the_title(); ?></h1>
 
-
+        <?php if ($image = get_field('image_recrutement')) : ?>
+            <div class="image-recrutement"'>
+            <?php echo wp_get_attachment_image($image['id'], 'large'); ?>
+            </div>
+        <?php endif; ?>
 
     </main><!-- #main -->
 <?php get_footer();
